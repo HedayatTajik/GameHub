c97f7c8752469015a620cc790a546e537c158c1b
Hedayatullah Tajik <HETAJIK@sma.de>
2025-11-23 12:37:26 +0100

    add models

---
+++ GameHub/Components/Models/Player.cs
@@ -0,0 +1,12 @@
+﻿namespace ImposterGame.Models
+{
+    public class Player
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public bool IsImposter { get; set; } = false;
+        public string Word { get; set; } = string.Empty;
+        public bool HasViewedCard { get; set; } = false;
+        public string Uri { get; set; } = string.Empty;
+    }
+}
